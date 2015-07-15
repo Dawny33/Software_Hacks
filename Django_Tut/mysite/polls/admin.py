fb7703b5c5ba0c733bace62c3058df5345a21762
Jalem Raj Rohit <rohit.raj@kayako.com>
2015-07-15 21:24:00 +0530

    Changed field sets

--- Django_Tut/mysite/polls/admin.py
+++ Django_Tut/mysite/polls/admin.py
@@ -3,10 +3,17 @@ from django.contrib import admin
 # Registering the models
 from .models import Question
 
-#For putting the pub_date above question_text in the admin panel
+#For adjusting the pub_date and question_text in the admin panel
 class QuestionAdmin(admin.ModelAdmin):
+    #Puts the pub_date above question_text
     #fields = ['pub_date', 'question_text']
-    
+
+    fieldsets = [
+        ("Question text",                  {'fields': ['question_text']}),
+        ('Date Information'   ,   {'fields': ['pub_date']}),
+    ]
+
+
 
 
 admin.site.register(Question, QuestionAdmin)
