9e8d451cf2c16069b1aa9acdf92c3316733a554d
Jalem Raj Rohit <rohit.raj@kayako.com>
2015-07-15 21:46:47 +0530

    Easy question creation

--- Django_Tut/mysite/polls/admin.py
+++ Django_Tut/mysite/polls/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 # Registering the models
-from .models import Question
+from .models import Choice, Question
+
+#For enabling to add choices when creating questions.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 
 #For adjusting the pub_date and question_text in the admin panel
 class QuestionAdmin(admin.ModelAdmin):
@@ -9,11 +15,15 @@ class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text']
 
     fieldsets = [
-        ("Question text",                  {'fields': ['question_text']}),
-        ('Date Information'   ,   {'fields': ['pub_date']}),
+        ("Question text"      ,   {'fields': ['question_text']}),
+        ('Date Information'   ,   {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
+
+    #For adding choices.
+    inlines = [ChoiceInline]
 
 
 
 
 admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
