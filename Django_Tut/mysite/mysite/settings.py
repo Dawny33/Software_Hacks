acd4930d85d4e14a17409ad1d4294fc9e01157a8
Jalem Raj Rohit <rohit.raj@kayako.com>
2015-07-16 20:49:33 +0530

    Included Django Templates

--- Django_Tut/mysite/mysite/settings.py
+++ Django_Tut/mysite/mysite/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        
+        #A list of filesystem to check when loading Django templates
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
